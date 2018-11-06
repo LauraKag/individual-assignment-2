157328105249e5de70be144dd16608d47a5a0b51
LauraKag <laura.kageneck@student.ie.edu>
2018-11-06 10:02:52 +0100

    added bubblesort

--- assignment.py
+++ assignment.py
@@ -5,3 +5,16 @@ Created on Tue Nov  6 10:00:24 2018
 @author: laura
 """
 
+def bubblesort(lst):
+    
+    for y in range(len(lst)-1):
+        
+        for x in range(len(lst)-y-1):
+            if lst[x] > lst[x+1]:
+                temp= lst[x]
+                lst[x]=lst[x+1]
+                lst[x+1]=temp
+        
+lst=[9,8,7,6,5,4]
+bubblesort(lst)
+print(lst)
